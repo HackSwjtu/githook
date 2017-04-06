c0e21a79931a25786329114f7aaf5ae530f58349
Desgard_Duan <desgard_duan@foxmail.com>
2017-04-06 10:50:15 +0800

    Test commit for <pre-commit>

--- src/commit-msg.py
+++ src/commit-msg.py
@@ -1,21 +1,3 @@
-#!/usr/bin python
+#!/usr/bin python3
 
-import sys, os, re
-from subprocess import check_output
-
-commit_msg_filepath = sys.argv[1]
-
-branch = check_output(['git', 'symbolic-ref', '--short', 'HEAD']).strip()
-print("commit-msg: On branch '%s'" % branch)
-
-if branch.startswith('issue-'):
-    print('commit-msg: Oh hey, it\'s an issue branch')
-    result = re.match('issue-(.*)', branch)
-    issue_number = result.group(1)
-    required_message = "ISSUE-%s" % issue_number
-
-    with open(commit_msg_filepath, 'r') as f:
-        content = f.read()
-        if not content.startswith(required_message):
-            print("commit-msg: ERROR! The commit message must start with '%s'" % required_message)
-            sys.exit(1)
+print('hello')
